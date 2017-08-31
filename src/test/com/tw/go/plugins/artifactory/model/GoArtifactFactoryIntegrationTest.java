b4dc447cdfaab8b768e58190db1c25a4a8dfd131
Douglas Quintanilha Barbosa Ferreira | Stone <dbferreira@stone.com.br>
2017-08-31 10:39:03 -0300

    Fixing some broken tests

--- src/test/com/tw/go/plugins/artifactory/model/GoArtifactFactoryIntegrationTest.java
+++ src/test/com/tw/go/plugins/artifactory/model/GoArtifactFactoryIntegrationTest.java
@@ -10,21 +10,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 import com.google.common.collect.ImmutableMap;
-
-import com.tw.go.plugins.artifactory.model.GoArtifact;
-import com.tw.go.plugins.artifactory.model.GoArtifactFactory;
 import com.tw.go.plugins.artifactory.task.config.Context;
 import com.tw.go.plugins.artifactory.task.config.TaskConfig;
 import com.tw.go.plugins.artifactory.task.config.TaskConfigBuilder;
-import com.tw.go.plugins.artifactory.task.executor.TaskExecutionContextBuilder;
 
 public class GoArtifactFactoryIntegrationTest {
     private static GoArtifactFactory factory;
 
     private Map<String, String> properties = ImmutableMap.<String, String>builder().put("name", "value").build();
-    private static Context context;
+    
+    		private static Context context;
     
     @BeforeClass
     public static void beforeAll() throws Exception {
@@ -34,16 +32,15 @@ public class GoArtifactFactoryIntegrationTest {
         context = new Context(envVars);
         factory = new GoArtifactFactory();
     }
-
-    //@Test 
+    
+    @Test 
     public void shouldCreateGoArtifacts() {
-//        TaskConfig configOld = new TaskConfigBuilder()
-//                .path(path("src", "test", "resources", "artifact.txt"))
-//                .uri("repo/path/to/output.ext")
-//                .property("name", "value")
-//                .build();
     	
-        TaskConfig config = new TaskConfig(config)
+    	Map<String, String> enviromentVariable = new HashMap<String, String>();
+    	enviromentVariable.put("ArtifactoryUri", "repo/path/to/output.ext");
+    	enviromentVariable.put("UriIsFolder", "false");
+    	enviromentVariable.put("ArtifactoryPath", path("src", "test", "resources", "artifact.txt"));    	
+        TaskConfig config = new TaskConfig(enviromentVariable);
 
         Collection<GoArtifact> artifacts = factory.createArtifacts(config, context);
 
@@ -52,14 +49,15 @@ public class GoArtifactFactoryIntegrationTest {
         ASSERT.that(artifacts).has().exactly(expectedArtifact);
     }
 
-    //@Test
+
+    @Test
     public void shouldCreateArtifactsWithUniqueRemotePathsIfUriIsAFolder() {
-        TaskConfig config = new TaskConfigBuilder()
-                .path(asPath("src", "test", "resources", "**{artifact.txt,test.html}"))
-                .uri("repo/path")
-                .uriIsFolder(true)
-                .property("name", "value")
-                .build();
+    	Map<String, String> enviromentVariable = new HashMap<>();
+    	enviromentVariable.put("ArtifactoryUri", "repo/path");
+    	enviromentVariable.put("UriIsFolder", "true");
+    	enviromentVariable.put("ArtifactoryPath", asPath("src", "test", "resources", "**{artifact.txt,test.html}"));
+    	TaskConfig config = new TaskConfig(enviromentVariable);
+    
 
         Collection<GoArtifact> artifacts = factory.createArtifacts(config, context);
 
@@ -69,15 +67,14 @@ public class GoArtifactFactoryIntegrationTest {
         ASSERT.that(artifacts).has().exactly(artifactTxt, testHtml);
     }
 
-    //@Test
+    @Test
     public void shouldCreateArtifactsWithSameRemotePathIfUriIsNotAFolder() {
-        TaskConfig config = new TaskConfigBuilder()
-                .path(asPath("src", "test", "resources", "**{artifact.txt,test.html}"))
-                .uri("repo/path")
-                .uriIsFolder(false)
-                .property("name", "value")
-                .build();
-
+    	Map<String, String> enviromentVariable = new HashMap<>();
+    	enviromentVariable.put("ArtifactoryUri", "repo/path");
+    	enviromentVariable.put("UriIsFolder", "false");
+    	enviromentVariable.put("ArtifactoryPath", asPath("src", "test", "resources", "**{artifact.txt,test.html}"));
+    	TaskConfig config = new TaskConfig(enviromentVariable);
+    	
         Collection<GoArtifact> artifacts = factory.createArtifacts(config, context);
 
         GoArtifact artifactTxt = goArtifact("src/test/resources/artifact.txt", "repo/path", properties);
@@ -88,13 +85,13 @@ public class GoArtifactFactoryIntegrationTest {
     
     //@Test
     public void shouldSubstituteEnvironmentVariablesIntoUri() {
-        TaskConfig config = new TaskConfigBuilder()
-                .path(asPath("src", "test", "resources", "**{artifact.txt,test.html}"))
-                .uri("repo/path/${GO_REVISION}")
-                .uriIsFolder(false)
-                .property("name", "value")
-                .build();
 
+    	Map<String, String> enviromentVariable = new HashMap<>();
+    	enviromentVariable.put("ArtifactoryUri", "repo/path/${GO_REVISION}");
+    	enviromentVariable.put("UriIsFolder", "false");
+    	enviromentVariable.put("ArtifactoryPath", asPath("src", "test", "resources", "**{artifact.txt,test.html}"));
+    	TaskConfig config = new TaskConfig(enviromentVariable);
+        
         Collection<GoArtifact> artifacts = factory.createArtifacts(config, context);
 
         GoArtifact artifactTxt = goArtifact("src/test/resources/artifact.txt", "repo/path/123", properties);
--- src/test/com/tw/go/plugins/artifactory/task/executor/PublishTaskExecutorIntegrationTest.java
+++ src/test/com/tw/go/plugins/artifactory/task/executor/PublishTaskExecutorIntegrationTest.java
@@ -1,32 +1,32 @@
 package com.tw.go.plugins.artifactory.task.executor;
 
-import com.thoughtworks.go.plugin.api.response.execution.ExecutionResult;
-import com.thoughtworks.go.plugin.api.task.TaskConfig;
-import com.thoughtworks.go.plugin.api.task.TaskExecutionContext;
+import static com.thoughtworks.webstub.StubServerFacade.newServer;
+import static com.thoughtworks.webstub.dsl.builders.ResponseBuilder.response;
+import static com.tw.go.plugins.artifactory.testutils.FilesystemUtils.delete;
+import static com.tw.go.plugins.artifactory.testutils.FilesystemUtils.path;
+import static com.tw.go.plugins.artifactory.testutils.FilesystemUtils.read;
+import static com.tw.go.plugins.artifactory.testutils.MapBuilder.map;
+import static org.truth0.Truth.ASSERT;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.thoughtworks.go.plugin.api.response.DefaultGoApiResponse;
 import com.thoughtworks.webstub.StubServerFacade;
 import com.thoughtworks.webstub.dsl.HttpDsl;
 import com.tw.go.plugins.artifactory.model.GoArtifactFactory;
 import com.tw.go.plugins.artifactory.model.GoBuildDetailsFactory;
-import com.tw.go.plugins.artifactory.task.config.TaskConfigBuilder;
+import com.tw.go.plugins.artifactory.model.Result;
+import com.tw.go.plugins.artifactory.task.config.Context;
+import com.tw.go.plugins.artifactory.task.config.TaskConfig;
 import com.tw.go.plugins.artifactory.task.publish.BuildArtifactPublisher;
-import com.tw.go.plugins.artifactory.testutils.FilesystemUtils;
-import org.apache.commons.io.IOUtils;
-import org.junit.*;
-
-import java.io.File;
-import java.io.IOException;
-
-import static com.thoughtworks.webstub.StubServerFacade.newServer;
-import static com.thoughtworks.webstub.dsl.builders.ResponseBuilder.response;
-import static com.tw.go.plugins.artifactory.testutils.FilesystemUtils.delete;
-import static com.tw.go.plugins.artifactory.testutils.FilesystemUtils.read;
-import static com.tw.go.plugins.artifactory.testutils.FilesystemUtils.path;
-import static com.tw.go.plugins.artifactory.testutils.MapBuilder.map;
-import static org.apache.commons.io.IOUtils.readLines;
-import static org.apache.commons.lang.StringUtils.join;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-import static org.truth0.Truth.ASSERT;
 
 public class PublishTaskExecutorIntegrationTest {
     private static StubServerFacade server;
@@ -44,7 +44,7 @@ public class PublishTaskExecutorIntegrationTest {
     @Before
     public void beforeEach() {
         artifactoryStub.reset();
-        artifactoryStub.get("/api/system/version").returns(response(200).withContent("{ \"version\" : \"3.2.1.1\" }"));
+        artifactoryStub.get("/api/system/version").returns(response(DefaultGoApiResponse.SUCCESS_RESPONSE_CODE).withContent("{ \"version\" : \"3.2.1.1\" }"));
 
         executor = new PublishTaskExecutor(new GoArtifactFactory(), new GoBuildDetailsFactory(), new BuildArtifactPublisher());
         pluginDirectory = path(System.getProperty("java.io.tmpdir"), "com.tw.go.plugins.go-artifactory-plugin");
@@ -52,23 +52,19 @@ public class PublishTaskExecutorIntegrationTest {
 
     @Test
     public void shouldUploadArtifactAndBuildDetails() throws IOException {
-        TaskConfig config = new TaskConfigBuilder()
-                .uri("test-repo/path/to/artifact.ext")
-                .path("src/test/resources/artifact.txt")
-                .property("a", "b")
-                .build();
 
-        TaskExecutionContext executionContext =
-                new TaskExecutionContextBuilder()
-                        .withWorkingDir(System.getProperty("java.io.tmpdir"))
-                        .withEnvVars(map("ARTIFACTORY_URL", "http://localhost:8888")
-                                .and("ARTIFACTORY_USER", "admin")
-                                .and("ARTIFACTORY_PASSWORD", "password")
-                                .and("GO_SERVER_URL", "http://localhost:8153/go")
-                                .and("GO_PIPELINE_NAME", "pipeline")
-                                .and("GO_PIPELINE_COUNTER", "1")
-                                .and("GO_STAGE_COUNTER", "3"))
-                        .build();
+        TaskConfig config = new TaskConfig("test-repo/path/to/artifact.ext",false, "src/test/resources/artifact.txt");
+        
+        Map<?,?> envVars = map("ARTIFACTORY_URL", "http://localhost:8888")
+                .and("ARTIFACTORY_USER", "admin")
+                .and("ARTIFACTORY_PASSWORD", "password")
+                .and("GO_SERVER_URL", "http://localhost:8153/go")
+                .and("GO_PIPELINE_NAME", "pipeline")
+                .and("GO_PIPELINE_COUNTER", "1")
+                .and("GO_STAGE_COUNTER", "3");
+        
+        Context context = new Context(envVars , System.getProperty("java.io.tmpdir") );
+        
 
         File testResponse = new File(path("src", "test", "resources", "uploadResponse.json"));
         artifactoryStub.put("/test-repo/path/to/artifact.ext").withContent("content")
@@ -78,9 +74,9 @@ public class PublishTaskExecutorIntegrationTest {
         artifactoryStub.put("/test-repo/path/to/artifact.ext.md5").withContent("9a0364b9e99bb480dd25e1f0284c8555").returns(response(201));
         artifactoryStub.put("/api/build").withHeader("Content-Type", "application/vnd.org.jfrog.artifactory+json").returns(response(204));
 
-        ExecutionResult result = executor.execute(config, executionContext);
+        Result result = executor.execute(config, context);
 
-        ASSERT.that(result.isSuccessful()).isTrue();
+        ASSERT.that(result.responseCode()).isEqualTo(DefaultGoApiResponse.SUCCESS_RESPONSE_CODE);
         ASSERT.that(new File(pluginDirectory, "uploadMetadata.json").exists()).isTrue();
     }
 
